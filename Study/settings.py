c4b184d55d6de6c978a87ebbfa2d83254f6bb4af
Peter-Oenga <poenga230@gmail.com>
2024-09-26 23:03:41 +0300

    SEO

--- Study/settings.py
+++ Study/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1xiqfyt!d)mp*=wgwwo#unz*y6yjdx_yozg)w05*_8cl%g5nf7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['4d55-197-231-179-166.ngrok-free.app', 'study-mart.onrender.com', 'localhost', '127.0.0.1']
 
@@ -125,12 +125,14 @@ USE_TZ = True
 
 
 
+# Media
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
--- core/views.py
+++ core/views.py
@@ -1,69 +1,82 @@
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
-from .forms import RegistrationForm
+from django.shortcuts import render
+from .models import Document
 
-# Create your views here.
-
+# View for Index Page Documents
 def index(request):
-    return render(request, "index.html")
+    documents = Document.objects.filter(page='index')
+    return render(request, 'index.html', {'documents': documents})
 
-
+# View for About Page Documents
 def about(request):
-    return render(request, "about.html")
+    documents = Document.objects.filter(page='about')
+    return render(request, 'about.html', {'documents': documents})
 
+# View for Services Page Documents
 def services(request):
-    return render(request, 'services.html')
+    documents = Document.objects.filter(page='services')
+    return render(request, 'services.html', {'documents': documents})
 
-
+# View for Register Page Documents
 def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()  # Save the user information to the database
-            return redirect('index')  # Redirect to the home page
-    else:
-        form = RegistrationForm()
-    return render(request, 'register.html', {'form': form})
+    documents = Document.objects.filter(page='register')
+    return render(request, 'register.html', {'documents': documents})
 
-
+# View for Reports Page Documents
 def reports(request):
-    return render(request, 'reports.html')
+    documents = Document.objects.filter(page='reports')
+    return render(request, 'reports.html', {'documents': documents})
 
-
+# View for CS Page Documents
 def cs(request):
-    return render(request, 'cs.html')
+    documents = Document.objects.filter(page='cs')
+    return render(request, 'cs.html', {'documents': documents})
 
-
-
+# View for ML Page Documents
 def ml(request):
-    return render(request, 'ml.html')
+    documents = Document.objects.filter(page='ml')
+    return render(request, 'ml.html', {'documents': documents})
 
-
-def data_analysis(request):
-    return render(request, 'data_analysis.html')
-
-
+# View for Economics Page Documents
 def economics(request):
-    return render(request, 'economics.html')
+    documents = Document.objects.filter(page='economics')
+    return render(request, 'economics.html', {'documents': documents})
 
+# View for Data Analysis Page Documents
+def data_analysis(request):
+    documents = Document.objects.filter(page='data_analysis')
+    return render(request, 'data_analysis.html', {'documents': documents})
+
+# View for Data Mining Page Documents
 def data_mining(request):
-    return render(request, 'data_mining.html')
+    documents = Document.objects.filter(page='data_mining')
+    return render(request, 'data_mining.html', {'documents': documents})
 
-
+# View for Finance Page Documents
 def finance(request):
-    return render(request, 'finance.html')
+    documents = Document.objects.filter(page='finance')
+    return render(request, 'finance.html', {'documents': documents})
 
+# View for Business Analytics Page Documents
 def business_analytics(request):
-    return render(request, 'business_analytics.html')
+    documents = Document.objects.filter(page='business_analytics')
+    return render(request, 'business_analytics.html', {'documents': documents})
 
+# View for Case Studies Page Documents
 def case_studies(request):
-    return render(request, 'case_studies.html')
+    documents = Document.objects.filter(page='case_studies')
+    return render(request, 'case_studies.html', {'documents': documents})
 
+# View for Business Page Documents
 def business(request):
-    return render(request, 'business.html')
+    documents = Document.objects.filter(page='business')
+    return render(request, 'business.html', {'documents': documents})
 
+# View for Marketing Page Documents
 def marketing(request):
-    return render(request, 'marketing.html')
+    documents = Document.objects.filter(page='marketing')
+    return render(request, 'marketing.html', {'documents': documents})
 
+# View for Research Page Documents
 def research(request):
-    return render(request, 'research.html')
+    documents = Document.objects.filter(page='research')
+    return render(request, 'research.html', {'documents': documents})
