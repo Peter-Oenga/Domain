26f50b47fb32032f55c9dbcc117c3372a740de85
Peter-Oenga <poenga230@gmail.com>
2024-10-08 14:35:49 +0300

    Included the allowed host

--- Study/settings.py
+++ Study/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1xiqfyt!d)mp*=wgwwo#unz*y6yjdx_yozg)w05*_8cl%g5nf7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['a114-197-231-179-166.ngrok-free.app', 'study-mart.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['study-mart-2.onrender.com', 'study-mart.onrender.com', 'localhost', '127.0.0.1']
 
 
 CSRF_TRUSTED_ORIGINS = [
