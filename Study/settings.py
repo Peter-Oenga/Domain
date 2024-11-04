8f81e1c56a923c3b4ab8f0182aa5f4c9ff52f370
Peter-Oenga <poenga230@gmail.com>
2024-11-04 15:33:57 +0300

    DEBUG=True

--- Study/settings.py
+++ Study/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1xiqfyt!d)mp*=wgwwo#unz*y6yjdx_yozg)w05*_8cl%g5nf7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['153.92.210.185', 'studysmartexperts.com']
 
