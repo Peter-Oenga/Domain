579c946381166d3595fddabc496302a5abe7713e
Peter-Oenga <poenga230@gmail.com>
2024-11-11 23:23:07 +0300

    Responsive home page

--- Study/settings.py
+++ Study/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1xiqfyt!d)mp*=wgwwo#unz*y6yjdx_yozg)w05*_8cl%g5nf7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['153.92.210.185', 'studysmartexperts.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['614b-105-161-17-235.ngrok-free.app', '153.92.210.185', 'studysmartexperts.com', 'localhost', '127.0.0.1', ]
+
 
 
 
